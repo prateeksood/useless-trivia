249098695a162e3d7947cacf27f084656018ab13
Prateek Sood <prateeksood123@gmail.com>
2019-12-08 18:59:09 +0530

    start

---
+++ src/UIComponents/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Footer=()=>{
+    return <p>footer..</p>
+}
+
+export default Footer
---
+++ src/UIComponents/MainBody.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainBody=()=>{
+
+    return(
+        <div>
+            <p>Question</p>
+            <button>option1</button>
+            <button>option2</button>
+            <button>option3</button>
+            <button>option4</button>
+        </div>
+    )
+}
+
+export default MainBody
